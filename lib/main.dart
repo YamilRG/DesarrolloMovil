6d1592492e63307151cebe31f1c5247763c9a22e
YamilRG <eduardo_1600@hotmail.com>
2022-02-03 15:47:26 -0600

    Cambio de texto

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Si presionas, aumentara el contador:',
+              'Si presionas, aumentara el numero:',
             ),
             Text(
               '$_counter',
